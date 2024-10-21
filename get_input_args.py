184f928343d7314b2d4ab95ee7d5831232de0e9e
Greedy Pinky <ritalaw04@gmail.com>
2024-10-20 21:58:58 -0700

    fixed bug for arg input

--- get_input_args.py
+++ get_input_args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
-#          from the user using the Argparse Python module. If the user fails to 
+#
+# PROGRAMMER:
+# DATE CREATED:
+# REVISED DATE:
+# PURPOSE: Create a function that retrieves the following 3 command line inputs
+#          from the user using the Argparse Python module. If the user fails to
 #          provide some or all of the 3 inputs, then the default values are
 #          used for the missing inputs. Command Line Arguments:
 #     1. Image Folder as --dir with default value 'pet_images'
@@ -16,6 +16,8 @@
 ##
 # Imports python modules
 import argparse
+
+
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
@@ -38,12 +40,15 @@ def get_input_args():
     parser = argparse.ArgumentParser(description='to pass 3 arguments.')
 
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    parser.add_argument('--dir', type=str, default='pet_images', help='need argument --dir')
-    parser.add_argument('--arch', type=str, default='vgg', help='need argument --arch')
-    parser.add_argument('--dogfile', type=str, default='dognames.txt', help = 'need argument --dogfile')
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    # data structure that stores the command line arguments object 
+    parser.add_argument('--dir', type=str,
+                        default='pet_images/', help='path to folder of images')
+    parser.add_argument('--arch', type=str, default='vgg',
+                        help='need argument --arch')
+    parser.add_argument('--dogfile', type=str,
+                        default='dognames.txt', help='need argument --dogfile')
+
+    # Replace None with parser.parse_args() parsed argument collection that
+    # you created with this function
+    # data structure that stores the command line arguments object
     args = parser.parse_args()
     return args
